e830f12bf9cfa68aa339bc508a21a2692aa65617
mantanmoksh <mantanmoksh@gmail.com>
2025-04-22 14:23:25 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,26 @@
-import mysql.connector
 import os
+import pymysql
+from dotenv import load_dotenv
 
+# Load environment variables from .env
+load_dotenv()
+
+# Function to get DB connection
 def get_connection():
-    return mysql.connector.connect(
-        host=os.environ.get('DB_HOST'),
-        user=os.environ.get('DB_USER'),
-        password=os.environ.get('DB_PASSWORD'),
-        database=os.environ.get('DB_NAME')
+    # Retrieve credentials from environment variables
+    host = os.getenv("DB_HOST", "switchyard.proxy.rlwy.net")
+    user = os.getenv("DB_USER", "root")
+    password = os.getenv("DB_PASSWORD", "vqUeTTDMFoOLPdJnNTHSwRuyHyeTKguZ")
+    db = os.getenv("DB_NAME", "railway")
+    port = int(os.getenv("DB_PORT", 28897))  # Make sure to set the port number
+
+    # Establish a connection using pymysql
+    connection = pymysql.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=db,
+        port=port,
+        cursorclass=pymysql.cursors.DictCursor
     )
+    return connection
